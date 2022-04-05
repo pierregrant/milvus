edbe0e952426a6c05ea1ed5e4fc9011bf982d0ed
Congqi Xia <congqi.xia@zilliz.com>
2022-04-05 11:11:51 +0800

    Add ShardSegmentDetector implementation
    Signed-off-by: Congqi Xia <congqi.xia@zilliz.com>

---
+++ internal/querynode/shard_segment_detector.go
@@ -0,0 +1,168 @@
+package querynode
+
+import (
+	"context"
+	"sync"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/milvus-io/milvus/internal/log"
+	"github.com/milvus-io/milvus/internal/proto/querypb"
+	"go.etcd.io/etcd/api/v3/mvccpb"
+	v3rpc "go.etcd.io/etcd/api/v3/v3rpc/rpctypes"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"go.uber.org/zap"
+)
+
+// etcdShardSegmentDetector watch etcd prefix for segment event.
+type etcdShardSegmentDetector struct {
+	client *clientv3.Client
+	path   string
+	evtCh  chan segmentEvent
+
+	wg        sync.WaitGroup
+	closeCh   chan struct{}
+	closeOnce sync.Once
+}
+
+// NewEtcdShardSegmentDetector returns a segmentDetector with provided etcd client and root path.
+func NewEtcdShardSegmentDetector(client *clientv3.Client, rootPath string) *etcdShardSegmentDetector {
+	return &etcdShardSegmentDetector{
+		client:  client,
+		path:    rootPath,
+		evtCh:   make(chan segmentEvent, 32),
+		closeCh: make(chan struct{}),
+	}
+}
+
+func (sd *etcdShardSegmentDetector) Close() {
+	sd.closeOnce.Do(func() {
+		close(sd.closeCh)
+		sd.wg.Wait()
+		close(sd.evtCh)
+	})
+}
+
+func (sd *etcdShardSegmentDetector) watchSegments(collectionID int64, replicaID int64, vchannelName string) ([]segmentEvent, <-chan segmentEvent) {
+	resp, err := sd.client.Get(context.Background(), sd.path, clientv3.WithPrefix())
+	if err != nil {
+		log.Warn("Etcd SegmentDetector get replica info failed", zap.Error(err))
+		panic(err)
+	}
+
+	var events []segmentEvent
+	for _, kv := range resp.Kvs {
+		info, err := sd.parseSegmentInfo(kv.Value)
+		log.Warn("cqx", zap.Any("info", info))
+		if err != nil {
+			log.Warn("SegmentDetector failed to parse segmentInfo", zap.Error(err))
+			continue
+		}
+		if info.CollectionID != collectionID || info.GetDmChannel() != vchannelName {
+			continue
+		}
+
+		if inList(info.GetReplicaIds(), replicaID) {
+			events = append(events, segmentEvent{
+				eventType: segmentAdd,
+				segmentID: info.GetSegmentID(),
+				nodeID:    info.GetNodeID(),
+				state:     segmentStateLoading,
+			})
+		}
+	}
+
+	sd.wg.Add(1)
+	watchCh := sd.client.Watch(context.Background(), sd.path, clientv3.WithRev(resp.Header.GetRevision()+1), clientv3.WithPrefix(), clientv3.WithPrevKV())
+	go sd.watch(watchCh, collectionID, replicaID)
+
+	return events, sd.evtCh
+}
+
+func (sd *etcdShardSegmentDetector) watch(ch clientv3.WatchChan, collectionID int64, replicaID int64) {
+	defer sd.wg.Done()
+	log.Debug("etcdSegmentDetector start watch")
+	for {
+		select {
+		case <-sd.closeCh:
+			log.Warn("Closed SegmentDetector watch loop quit", zap.Int64("collectionID", collectionID), zap.Int64("replicaID", replicaID))
+			return
+		case evt, ok := <-ch:
+			if !ok {
+				log.Warn("SegmentDetector event channel closed")
+				return
+			}
+			if err := evt.Err(); err != nil {
+				if err == v3rpc.ErrCompacted {
+
+				}
+			}
+			for _, e := range evt.Events {
+				switch e.Type {
+				case mvccpb.PUT:
+					sd.handlePutEvent(e, collectionID, replicaID)
+				case mvccpb.DELETE:
+					sd.handleDelEvent(e, collectionID, replicaID)
+				}
+			}
+		}
+	}
+}
+
+func (sd *etcdShardSegmentDetector) handlePutEvent(e *clientv3.Event, collectionID int64, replicaID int64) {
+	info, err := sd.parseSegmentInfo(e.Kv.Value)
+	if err != nil {
+		log.Warn("Segment detector failed to parse event", zap.Any("event", e), zap.Error(err))
+		return
+	}
+
+	if info.GetCollectionID() != collectionID || inList(info.GetReplicaIds(), replicaID) {
+		// ignore not match events
+		return
+	}
+	/*
+		var prevInfo *querypb.SegmentInfo
+		if e.PrevKv != nil {
+			prevInfo, _ = sd.parseSegmentInfo(e.PrevKv.Value)
+		}*/
+
+	sd.evtCh <- segmentEvent{
+		eventType: segmentAdd,
+		segmentID: info.GetSegmentID(),
+		nodeID:    info.GetNodeID(),
+		state:     segmentStateLoading,
+	}
+}
+
+func (sd *etcdShardSegmentDetector) handleDelEvent(e *clientv3.Event, collectionID int64, replicaID int64) {
+	if e.PrevKv == nil {
+		return
+	}
+	info, err := sd.parseSegmentInfo(e.PrevKv.Value)
+	if err != nil {
+		log.Warn("SegmentDetector failed to parse delete event", zap.Any("event", e), zap.Error(err))
+		return
+	}
+
+	sd.evtCh <- segmentEvent{
+		eventType: segmentDel,
+		segmentID: info.GetSegmentID(),
+		nodeID:    info.GetNodeID(),
+		state:     segmentStateOffline,
+	}
+}
+
+// TODO maybe should use other proto
+func (sd *etcdShardSegmentDetector) parseSegmentInfo(bs []byte) (*querypb.SegmentInfo, error) {
+	info := &querypb.SegmentInfo{}
+	err := proto.Unmarshal(bs, info)
+	return info, err
+}
+
+func inList(list []int64, target int64) bool {
+	for _, i := range list {
+		if i == target {
+			return true
+		}
+	}
+	return false
+}
