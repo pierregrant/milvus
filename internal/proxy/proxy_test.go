a0912020f51b5eafbb576f12be06df4a767a98f3
sunby <bingyi.sun@zilliz.com>
2022-04-06 16:33:11 +0800

    fix ut bugs

--- internal/proxy/proxy_test.go
+++ internal/proxy/proxy_test.go
@@ -1685,7 +1685,7 @@ func TestProxy(t *testing.T) {
 			DbName:         dbName,
 			CollectionName: collectionName,
 			PartitionNames: []string{partitionName},
-			ReplicaNumber: 1,
+			ReplicaNumber:  1,
 		})
 		assert.NoError(t, err)
 		assert.Equal(t, commonpb.ErrorCode_Success, resp.ErrorCode)
@@ -1696,7 +1696,7 @@ func TestProxy(t *testing.T) {
 			DbName:         dbName,
 			CollectionName: collectionName,
 			PartitionNames: []string{otherPartitionName},
-			ReplicaNumber: 1,
+			ReplicaNumber:  1,
 		})
 		assert.NoError(t, err)
 		assert.NotEqual(t, commonpb.ErrorCode_Success, resp.ErrorCode)
@@ -1707,7 +1707,7 @@ func TestProxy(t *testing.T) {
 			DbName:         dbName,
 			CollectionName: otherCollectionName,
 			PartitionNames: []string{partitionName},
-			ReplicaNumber: 1,
+			ReplicaNumber:  1,
 		})
 		assert.NoError(t, err)
 		assert.NotEqual(t, commonpb.ErrorCode_Success, resp.ErrorCode)
