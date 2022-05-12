c3089f4c449e69ac0f176d7c007f6f2535cd01b3
congqixia <congqi.xia@zilliz.com>
2022-05-12 18:09:53 +0800

    Fix channelUnsubscribe data race and logic (#16946)
    - Add a RWMutex for container/list which is not goroutine-safe
    - Fix the element in list is never removed

--- internal/querycoord/channel_unsubscribe.go
+++ internal/querycoord/channel_unsubscribe.go
@@ -42,6 +42,7 @@ type channelUnsubscribeHandler struct {
 	kvClient *etcdkv.EtcdKV
 	factory  msgstream.Factory
 
+	mut          sync.RWMutex // mutex for channelInfos, since container/list is not goroutine-safe
 	channelInfos *list.List
 	downNodeChan chan int64
 
@@ -70,6 +71,13 @@ func newChannelUnsubscribeHandler(ctx context.Context, kv *etcdkv.EtcdKV, factor
 	return handler, nil
 }
 
+// appendUnsubInfo pushes unsub info safely
+func (csh *channelUnsubscribeHandler) appendUnsubInfo(info *querypb.UnsubscribeChannelInfo) {
+	csh.mut.Lock()
+	defer csh.mut.Unlock()
+	csh.channelInfos.PushBack(info)
+}
+
 // reloadFromKV reload unsolved channels to unsubscribe
 func (csh *channelUnsubscribeHandler) reloadFromKV() error {
 	log.Debug("start reload unsubscribe channelInfo from kv")
@@ -83,7 +91,7 @@ func (csh *channelUnsubscribeHandler) reloadFromKV() error {
 		if err != nil {
 			return err
 		}
-		csh.channelInfos.PushBack(channelInfo)
+		csh.appendUnsubInfo(channelInfo)
 		csh.downNodeChan <- channelInfo.NodeID
 	}
 
@@ -99,11 +107,14 @@ func (csh *channelUnsubscribeHandler) addUnsubscribeChannelInfo(info *querypb.Un
 	}
 	// when queryCoord is restarted multiple times, the nodeID of added channelInfo may be the same
 	hasEnqueue := false
+	// reduce the lock range to iteration here, since `addUnsubscribeChannelInfo` is called one by one
+	csh.mut.RLock()
 	for e := csh.channelInfos.Back(); e != nil; e = e.Prev() {
 		if e.Value.(*querypb.UnsubscribeChannelInfo).NodeID == nodeID {
 			hasEnqueue = true
 		}
 	}
+	csh.mut.RUnlock()
 
 	if !hasEnqueue {
 		channelInfoKey := fmt.Sprintf("%s/%d", unsubscribeChannelInfoPrefix, nodeID)
@@ -111,7 +122,7 @@ func (csh *channelUnsubscribeHandler) addUnsubscribeChannelInfo(info *querypb.Un
 		if err != nil {
 			panic(err)
 		}
-		csh.channelInfos.PushBack(info)
+		csh.appendUnsubInfo(info)
 		csh.downNodeChan <- info.NodeID
 		log.Debug("add unsubscribeChannelInfo to handler", zap.Int64("nodeID", info.NodeID))
 	}
@@ -126,7 +137,10 @@ func (csh *channelUnsubscribeHandler) handleChannelUnsubscribeLoop() {
 			log.Debug("channelUnsubscribeHandler ctx done, handleChannelUnsubscribeLoop end")
 			return
 		case <-csh.downNodeChan:
+			csh.mut.RLock()
+			e := csh.channelInfos.Front()
 			channelInfo := csh.channelInfos.Front().Value.(*querypb.UnsubscribeChannelInfo)
+			csh.mut.RUnlock()
 			nodeID := channelInfo.NodeID
 			for _, collectionChannels := range channelInfo.CollectionChannels {
 				collectionID := collectionChannels.CollectionID
@@ -140,7 +154,11 @@ func (csh *channelUnsubscribeHandler) handleChannelUnsubscribeLoop() {
 				log.Error("remove unsubscribe channelInfo from etcd failed", zap.Int64("nodeID", nodeID))
 				panic(err)
 			}
-			log.Debug("unsubscribe channels success", zap.Int64("nodeID", nodeID))
+
+			csh.mut.Lock()
+			csh.channelInfos.Remove(e)
+			csh.mut.Unlock()
+			log.Info("unsubscribe channels success", zap.Int64("nodeID", nodeID))
 		}
 	}
 }
